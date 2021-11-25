09b662015b98ed4a910a107e032d3acbac7215aa
Justin Greywolf <justin.greywolf@lostware.com>
2021-11-24 17:10:07 -0800

    Removing archive files that shouldnt have been uploaded

--- archive/Skykick.MarsRover.Library/Exceptions/CollisionException.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Skykick.MarsRover.Library.Exceptions
-{
-    public class CollisionException : Exception
-    {
-        public CollisionException(string message) : base(message) { }
-    }
-}
